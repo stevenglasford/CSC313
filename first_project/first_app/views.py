a1c8451660df44c5f185968d4ab96938b1a31652
stevenglasford <stevenglasford@gmail.com>
2020-04-24 19:23:33 -0500

    Add work for class for day 4-24-2020

---
+++ first_project/first_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    #return HttpResponse("<strong><em>Hello World</em></strong>")
+    my_dict={'insert_me': 'Hello, I am from view.py'}
+    return render(request, 'first_app/index.html', context=my_dict)
